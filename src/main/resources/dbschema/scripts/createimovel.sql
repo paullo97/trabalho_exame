233b14defd039a7d9833606bb606f56b8199e532
paullo97 <paulo.cesar97@hotmail.com>
2018-12-14 20:38:30 -0200

    Sql imovel alterado mudando colunas

--- src/main/resources/dbschema/scripts/createimovel.sql
+++ src/main/resources/dbschema/scripts/createimovel.sql
@@ -2,6 +2,9 @@ create table if not exist lab04.imovel(
 	imo_id int not null, 
 	imo_cep varchar(10) not null, 
 	imo_numero int (6),
+	imo_bairro varchar(30) not null, 
+	imo_cidade varchar(30) not null, 
+	imo_estado varchar(20) not null, 
 	pro_id int not null,
 	primary key(imo_id)
 	FOREIGN KEY (pro_id) REFERENCES proprietario(pro_id))
